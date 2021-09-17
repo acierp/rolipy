dd9a1fa434fc44ab25063acbf3cc355cd0627c4f
Acier <61130011+AcierP@users.noreply.github.com>
2021-09-17 10:22:15 -0700

    Update rpi.py

--- rpi/rpi.py
+++ rpi/rpi.py
@@ -16,5 +16,21 @@ class RPI:
                     raise ConnectionError("Proxy format " + proxytype + " is not supported")
                 else:
                     raise ConnectionError("Proxy format is unknown or unsupported")
+    def idtoName(assetid, proxy=None):
+        if proxy == None:
+            itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails")
+            print(itemdetails.text)
+            #for item in itemdetails.json():
+        else: 
+            if proxy.startswith("https://"):
+                itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"https": proxy})
+            elif proxy.startswith("http://") or proxy.startswith('socks5://'):
+                itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"http": proxy})
+            else:
+                if "://" in proxy:
+                    proxytype = proxy.split('://')[0]
+                    raise ConnectionError("Proxy format " + proxytype + " is not supported")
+                else:
+                    raise ConnectionError("Proxy format is unknown or unsupported")
 
 value = RPI.getValue('1111', proxy='http://hihihi')
