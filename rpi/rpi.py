7a15700dd41c094011a1404e87d3529119cb0a3f
AcierP <aidancharlesstone@gmail.com>
2021-09-17 23:41:25 -0700

    Update rpi.py
    parsing changes

--- rpi/rpi.py
+++ rpi/rpi.py
@@ -1,18 +1,36 @@
 import requests, json, string
 class RPI:
+    def getlimitedsCatalog(item, format=None):
+        pass
+
     def getValue(item, proxy=None):
         if not item.isdigit():
             pass
-            #id to name here
         if proxy == None:
-            itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails")
-            print(itemdetails.text)
-            #for item in itemdetails.json():
+            itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"https": proxy})
+            itemcheck = itemdetails.json()['items'][str(item)][3]
+
+            if str(itemcheck) != "-1":
+                return itemdetails.json()['items'][str(item)][3]
+            else:
+                return itemdetails.json()['items'][str(item)][2]
         else: 
             if proxy.startswith("https://"):
                 itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"https": proxy})
+                itemcheck = itemdetails.json()['items'][str(item)][3]
+
+                if str(itemcheck) != "-1":
+                    return itemdetails.json()['items'][str(item)][3]
+                else:
+                    return itemdetails.json()['items'][str(item)][2]
             elif proxy.startswith("http://") or proxy.startswith('socks5://'):
                 itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"http": proxy})
+                itemcheck = itemdetails.json()['items'][str(item)][3]
+
+                if str(itemcheck) != "-1":
+                    return itemdetails.json()['items'][str(item)][3]
+                else:
+                    return itemdetails.json()['items'][str(item)][2]
             else:
                 if "://" in proxy:
                     proxytype = proxy.split('://')[0]
@@ -22,13 +40,15 @@ class RPI:
     def idtoName(item, proxy=None):
         if proxy == None:
             itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails")
-            print(itemdetails.text)
+            return itemdetails.json()['items'][str(item)][0]
             #for item in itemdetails.json():
         else: 
             if proxy.startswith("https://"):
                 itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"https": proxy})
+                return itemdetails.json()['items'][str(item)][0]
             elif proxy.startswith("http://") or proxy.startswith('socks5://'):
                 itemdetails = requests.get("https://www.rolimons.com/itemapi/itemdetails", proxies = {"http": proxy})
+                return itemdetails.json()['items'][str(item)][0]
             else:
                 if "://" in proxy:
                     proxytype = proxy.split('://')[0]
@@ -36,4 +56,5 @@ class RPI:
                 else:
                     raise ConnectionError("Proxy format is unknown or unsupported")
 
-#value = RPI.getValue('1111', proxy='http://hihihi')
+value = RPI.getValue('')
+print(value)
